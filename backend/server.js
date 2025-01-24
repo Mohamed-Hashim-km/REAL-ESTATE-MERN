ad2abf7cb251dbe94625e4ae0b5ee532d0306e91
Mohamed-Hashim-km <hashimhx3@gmail.com>
2025-01-25 00:46:42 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+connectDB();
+// db
+
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Realestate");
+});
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});  
